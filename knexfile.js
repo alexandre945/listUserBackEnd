c4231496a8252151165a4ee75cc0ee49fcd38625
alexandre <alexandresousaroberto@gmail.com>
2024-07-07 11:23:49 -0300

    configurando knexfile para o banco de dados

--- knexfile.js
+++ knexfile.js
@@ -2,13 +2,13 @@ module.exports = {
     development: {
       client: 'pg',
       connection: {
-        connectionString: 'postgresql://db_user_list_owner:39DlhjpUiaEH@ep-purple-pine-a5h15efe.us-east-2.aws.neon.tech/db_user_list?sslmode=require',
-        ssl: {
-          rejectUnauthorized: false
-        }
+        host: 'ep-purple-pine-a5h15efe.us-east-2.aws.neon.tech',
+        user: 'db_user_list_owner',
+        password: '39DlhjpUiaEH',
+        database: 'db_user_list',
+        ssl: { rejectUnauthorized: false }  // Adicione isso para aceitar conexões SSL
       },
       migrations: {
-        tableName: 'knex_migrations',
         directory: './migrations'
       }
     }
